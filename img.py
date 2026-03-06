e238a5f1d21aefaf638449125da6c364be3c1803
Hime143 <cleo.feingold@gmail.com>
2026-03-06 10:10:53 +0100

    Interpreter

---
+++ img.py
@@ -0,0 +1,65 @@
+import pygame
+
+#BACKGROUND
+background = pygame.image.load("Images/Banquise.png").convert_alpha()
+background = pygame.transform.scale(background, (1280,700))
+
+#COLISION
+collision_map = pygame.image.load("Images/collision_map.png").convert_alpha()
+collision_map = pygame.transform.scale(collision_map, (1280,700))
+
+#STANDING IMG
+img_up = pygame.image.load("Images/BOUBOUHAUT.png").convert_alpha()
+img_up = pygame.transform.scale(img_up, (70,128))
+
+img_down = pygame.image.load("Images/BOUBOUVERSNOUS.png").convert_alpha()
+img_down = pygame.transform.scale(img_down, (70,128))
+
+img_left = pygame.image.load("Images/BOUBOUGAUCHE.png").convert_alpha()
+img_left = pygame.transform.scale(img_left, (70,128))
+
+img_right = pygame.image.load("Images/BOUBOUDROITE.png").convert_alpha()
+img_right = pygame.transform.scale(img_right, (70,128))
+
+boubou_img = img_down
+
+#WALKING
+img_up1 = pygame.image.load("Images/BOUBOUDERRIERE1.png").convert_alpha()
+img_up1 = pygame.transform.scale(img_up1, (70,128))
+
+img_up2 = pygame.image.load("Images/BOUBOUDERRIERE2.png").convert_alpha()
+img_up2 = pygame.transform.scale(img_up2, (70,128))
+
+img_down1 = pygame.image.load("Images/BOUBOUDEVANT1.png").convert_alpha()
+img_down1 = pygame.transform.scale(img_down1, (70,128))
+
+img_down2 = pygame.image.load("Images/BOUBOUDEVANT2.png").convert_alpha()
+img_down2 = pygame.transform.scale(img_down2, (70,128))
+
+img_left1 = pygame.image.load("Images/BOUBOUGAUCHE1.png").convert_alpha()
+img_left1 = pygame.transform.scale(img_left1, (70,128))
+
+img_left2 = pygame.image.load("Images/BOUBOUGAUCHE2.png").convert_alpha()
+img_left2 = pygame.transform.scale(img_left2, (70,128))
+
+img_right1 = pygame.image.load("Images/BOUBOUDROITE1.png").convert_alpha()
+img_right1 = pygame.transform.scale(img_right1, (70,128))
+
+img_right2 = pygame.image.load("Images/BOUBOUDROITE2.png").convert_alpha()
+img_right2 = pygame.transform.scale(img_right2, (70,128))
+
+#ICONE
+maison = pygame.image.load("Images/maison_icone.png").convert_alpha()
+maison = pygame.transform.scale(maison, (60,60))
+
+musique = pygame.image.load("Images/musique_icone.png").convert_alpha()
+musique = pygame.transform.scale(musique, (60,60))
+
+parametre = pygame.image.load("Images/parametre_icone.png").convert_alpha()
+parametre = pygame.transform.scale(parametre, (60,60))
+
+peche = pygame.image.load("Images/peche_icone.png").convert_alpha()
+peche = pygame.transform.scale(peche, (60,60))
+
+poubelle = pygame.image.load("Images/poubelle_icone.png").convert_alpha()
+poubelle = pygame.transform.scale(poubelle, (60,60))
--- main.py
+++ main.py
@@ -6,51 +6,14 @@ pygame.display.set_caption("IceGuardian")
 clock = pygame.time.Clock()
 running = True
 speed = 3
-
-background = pygame.image.load("Images/Banquise.png").convert_alpha()
-background = pygame.transform.scale(background, (1280,700))
-
-collison_map = pygame.image.load("Images/collision_map.png").convert_alpha()
-collison_map = pygame.transform.scale(collison_map, (1280,700))
-img_up = pygame.image.load("Images/BOUBOUHAUT.png").convert_alpha()
-img_up = pygame.transform.scale(img_up, (70,128))
-
-img_down = pygame.image.load("Images/BOUBOUVERSNOUS.png").convert_alpha()
-img_down = pygame.transform.scale(img_down, (70,128))
-
-img_left = pygame.image.load("Images/BOUBOUGAUCHE.png").convert_alpha()
-img_left = pygame.transform.scale(img_left, (70,128))
-
-img_right = pygame.image.load("Images/BOUBOUDROITE.png").convert_alpha()
-img_right = pygame.transform.scale(img_right, (70,128))
-
-boubou_img = img_down
-
-img_up1 = pygame.image.load("Images/BOUBOUDERRIERE1.png").convert_alpha()
-img_up1 = pygame.transform.scale(img_up1, (70,128))
-
-img_up2 = pygame.image.load("Images/BOUBOUDERRIERE2.png").convert_alpha()
-img_up2 = pygame.transform.scale(img_up2, (70,128))
-
-img_down1 = pygame.image.load("Images/BOUBOUDEVANT1.png").convert_alpha()
-img_down1 = pygame.transform.scale(img_down1, (70,128))
-
-img_down2 = pygame.image.load("Images/BOUBOUDEVANT2.png").convert_alpha()
-img_down2 = pygame.transform.scale(img_down2, (70,128))
-
-img_left1 = pygame.image.load("Images/BOUBOUGAUCHE1.png").convert_alpha()
-img_left1 = pygame.transform.scale(img_left1, (70,128))
-
-img_left2 = pygame.image.load("Images/BOUBOUGAUCHE2.png").convert_alpha()
-img_left2 = pygame.transform.scale(img_left2, (70,128))
-
-img_right1 = pygame.image.load("Images/BOUBOUDROITE1.png").convert_alpha()
-img_right1 = pygame.transform.scale(img_right1, (70,128))
-
-img_right2 = pygame.image.load("Images/BOUBOUDROITE2.png").convert_alpha()
-img_right2 = pygame.transform.scale(img_right2, (70,128))
+from img import *
 
 boubou_rect = boubou_img.get_rect(center=(640, 350))
+maison_rect = maison.get_rect(center=(240, 150))
+musique_rect = musique.get_rect(center=(340, 200))
+parametre_rect = parametre.get_rect(center=(440, 250))
+peche_rect = peche.get_rect(center=(540, 300))
+poubelle_rect = poubelle.get_rect(center=(640, 350))
 
 direction = "down"
 frame_index = 0
@@ -75,7 +38,7 @@ while running:
         foot_x = boubou_rect.centerx
         foot_y = boubou_rect.bottom
 
-        if collison_map.get_at((foot_x, foot_y))[:3] == (0, 0, 0):
+        if collision_map.get_at((foot_x, foot_y))[:3] == (0, 0, 0):
             boubou_rect.y = old_y
 
     if keys[pygame.K_s] or keys[pygame.K_DOWN]:
@@ -87,7 +50,7 @@ while running:
         foot_x = boubou_rect.centerx
         foot_y = boubou_rect.bottom
 
-        if collison_map.get_at((foot_x, foot_y))[:3] == (0, 0, 0):
+        if collision_map.get_at((foot_x, foot_y))[:3] == (0, 0, 0):
             boubou_rect.y = old_y
 
     if keys[pygame.K_q] or keys[pygame.K_LEFT]:
@@ -99,7 +62,7 @@ while running:
         foot_x = boubou_rect.centerx
         foot_y = boubou_rect.bottom
 
-        if collison_map.get_at((foot_x, foot_y))[:3] == (0, 0, 0):
+        if collision_map.get_at((foot_x, foot_y))[:3] == (0, 0, 0):
             boubou_rect.x = old_x
 
     if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
@@ -111,7 +74,7 @@ while running:
         foot_x = boubou_rect.centerx
         foot_y = boubou_rect.bottom
 
-        if collison_map.get_at((foot_x, foot_y))[:3] == (0, 0, 0):
+        if collision_map.get_at((foot_x, foot_y))[:3] == (0, 0, 0):
             boubou_rect.x = old_x
 
     if moving:
@@ -125,7 +88,7 @@ while running:
                 frames = [img_down1, img_down2]
             elif direction == "left":
                 frames = [img_left1, img_left2]
-            elif direction == "right":
+            else:
                 frames = [img_right1, img_right2]
 
             frame_index = (frame_index + 1) % len(frames)
@@ -142,6 +105,11 @@ while running:
 
     screen.blit(background, (0,0))
     screen.blit(boubou_img, boubou_rect)
+    screen.blit(maison, maison_rect)
+    screen.blit(peche, peche_rect)
+    screen.blit(poubelle, poubelle_rect)
+    screen.blit(musique, musique_rect)
+    screen.blit(parametre, parametre_rect)
     pygame.display.flip()
 
 pygame.quit()
