8ad527c8a32c6279588902368f1cb1720ff4f805
Monica <fangmonica2007@gmail.com>
2026-03-18 11:04:22 +0100

    problème de lague

--- peche/poissons.py
+++ peche/poissons.py
@@ -5,20 +5,18 @@ import math
 WIDTH = 1280
 HEIGHT = 700
 
-
 class Poisson:
 
     def __init__(self):
 
-        # tailles augmentées : 60, 80, 100 au lieu de 20, 30, 40
         self.taille = random.choice([60, 80, 100])
         self.nb_point = self.taille // 20
 
         self.spawn()
 
-        # hitbox centrée sur le poisson
         self.rect = pygame.Rect(0, 0, self.taille * 2, self.taille)
         self.rect.center = (int(self.x), int(self.y))
+
         self.couleur = random.choice([
             (0, 200, 255),
             (255, 140, 0),
@@ -27,8 +25,62 @@ class Poisson:
             (255, 80, 80),
             (255, 220, 50),
         ])
-
         self.couleur_sombre = tuple(max(0, c - 60) for c in self.couleur)
+
+        # on charge l'image une seule fois ici
+        self.image = self._charger_image()
+
+    def _charger_image(self):
+        """Charge la bonne image selon la taille et la couleur"""
+
+        # petit poisson
+        if self.taille == 60:
+            if self.couleur == (255, 140, 0):
+                nom = "pet_poisson_bleu.png"
+            elif self.couleur == (100, 220, 100):
+                nom = "pet_poisson_jaune.png"
+            elif self.couleur == (220, 80, 180):
+                nom = "pet_poisson_orange.png"
+            elif self.couleur == (255, 80, 80):
+                nom = "pet_poisson_rose.png"
+            elif self.couleur == (255, 220, 50):
+                nom = "pet_poisson_rouge.png"
+            else:
+                nom = "pet_poisson_vert.png"
+
+        # moyen poisson
+        elif self.taille == 80:
+            if self.couleur == (0, 200, 255):
+                nom = "moy_poissons_bleu.png"
+            elif self.couleur == (100, 220, 100):
+                nom = "moy_poissons_jaune.png"
+            elif self.couleur == (220, 80, 180):
+                nom = "moy_poissons_orange.png"
+            elif self.couleur == (255, 220, 50):
+                nom = "moy_poissons_rose.png"
+            elif self.couleur == (255, 140, 0):
+                nom = "moy_poissons_rouge.png"
+            else:
+                nom = "moy_poissons_vert.png"
+
+        # grand poisson
+        else:
+            if self.couleur == (255, 140, 0):
+                nom = "gr_poissons_bleu.png"
+            elif self.couleur == (0, 200, 255):
+                nom = "gr_poissons_jaune.png"
+            elif self.couleur == (255, 80, 80):
+                nom = "gr_poissons_vert.png"
+            elif self.couleur == (220, 80, 180):
+                nom = "gr_poissons_orangepng.png"
+            elif self.couleur == (255, 220, 50):
+                nom = "gr_poissons_rose.png"
+            else:
+                nom = "gr_poissons_rouge.png"
+
+        # on charge et redimensionne une seule fois
+        img = pygame.image.load(f"Images/{nom}").convert_alpha()
+        return pygame.transform.smoothscale(img, (self.taille, 50))
 
     def spawn(self):
         self.x = -120.0
@@ -46,83 +98,5 @@ class Poisson:
         return self.x > WIDTH + 150
 
     def draw(self, screen):
-
-        #petit poisson
-        if self.taille == 60 :
-            if self.couleur == (255, 140, 0):
-                pp1 = pygame.image.load("Images/pet_poisson_bleu.png").convert_alpha()
-                pp1 = pygame.transform.scale(pp1, (self.taille, 50))
-                screen.blit(pp1, self.rect)
-            if self.couleur == (100, 220, 100):
-                pp2 = pygame.image.load("Images/pet_poisson_jaune.png").convert_alpha()
-                pp2 = pygame.transform.scale(pp2, (self.taille, 50))
-                screen.blit(pp2, self.rect)
-            if self.couleur == (220, 80, 180):
-                pp3 = pygame.image.load("Images/pet_poisson_orange.png").convert_alpha()
-                pp3 = pygame.transform.scale(pp3, (self.taille, 50))
-                screen.blit(pp3, self.rect)
-            if self.couleur == (255, 80, 80):
-                pp4 = pygame.image.load("Images/pet_poisson_rose.png").convert_alpha()
-                pp4 = pygame.transform.scale(pp4, (self.taille, 50))
-                screen.blit(pp4, self.rect)
-            if self.couleur == (255, 220, 50):
-                pp5 = pygame.image.load("Images/pet_poisson_rouge.png").convert_alpha()
-                pp5 = pygame.transform.scale(pp5, (self.taille, 50))
-                screen.blit(pp5, self.rect)
-            if self.couleur == (0, 200, 255):
-                pp6 = pygame.image.load("Images/pet_poisson_vert.png").convert_alpha()
-                pp6 = pygame.transform.scale(pp6, (self.taille, 50))
-                screen.blit(pp6, self.rect)
-         # moyen
-        if self.taille == 80:
-            if self.couleur == (0, 200, 255):
-                pm1 = pygame.image.load("Images/moy_poissons_bleu.png").convert_alpha()
-                pm1 = pygame.transform.scale(pm1, (self.taille, 50))
-                screen.blit(pm1, self.rect)
-            if self.couleur == (100, 220, 100):
-                pm2 = pygame.image.load("Images/moy_poissons_jaune.png").convert_alpha()
-                pm2 = pygame.transform.scale(pm2, (self.taille, 50))
-                screen.blit(pm2, self.rect)
-            if self.couleur == (220, 80, 180):
-                pm3 = pygame.image.load("Images/moy_poissons_orange.png").convert_alpha()
-                pm3 = pygame.transform.scale(pm3, (self.taille, 50))
-                screen.blit(pm3, self.rect)
-            if self.couleur == (255, 220, 50):
-                pm4 = pygame.image.load("Images/moy_poissons_rose.png").convert_alpha()
-                pm4 = pygame.transform.scale(pm4, (self.taille, 50))
-                screen.blit(pm4, self.rect)
-            if self.couleur == (255, 140, 0):
-                pm5 = pygame.image.load("Images/moy_poissons_rouge.png").convert_alpha()
-                pm5 = pygame.transform.scale(pm5, (self.taille, 50))
-                screen.blit(pm5, self.rect)
-            if self.couleur == (255, 80, 80):
-                pm6 = pygame.image.load("Images/moy_poissons_vert.png").convert_alpha()
-                pm6 = pygame.transform.scale(pm6, (self.taille, 50))
-                screen.blit(pm6, self.rect)
-
-        if self.taille == 100:
-            if self.couleur == (255, 140, 0):
-                pg1 = pygame.image.load("Images/gr_poissons_bleu.png").convert_alpha()
-                pg1 = pygame.transform.scale(pg1, (self.taille, 50))
-                screen.blit(pg1, self.rect)
-            if self.couleur == (0, 200, 255):
-                pg2 = pygame.image.load("Images/gr_poissons_jaune.png").convert_alpha()
-                pg2 = pygame.transform.scale(pg2, (self.taille, 50))
-                screen.blit(pg2, self.rect)
-
-            if self.couleur == (255, 80, 80):
-                pg6 = pygame.image.load("Images/gr_poissons_vert.png").convert_alpha()
-                pg6 = pygame.transform.scale(pg6, (self.taille, 50))
-                screen.blit(pg6, self.rect)
-            if self.couleur == (220, 80, 180):
-                pg3 = pygame.image.load("Images/gr_poissons_orangepng.png").convert_alpha()
-                pg3 = pygame.transform.scale(pg3, (self.taille, 50))
-                screen.blit(pg3, self.rect)
-            if self.couleur == (255, 220, 50):
-                pg4 = pygame.image.load("Images/gr_poissons_rose.png").convert_alpha()
-                pg4 = pygame.transform.scale(pg4, (self.taille, 50))
-                screen.blit(pg4, self.rect)
-            if self.couleur == (100, 220, 100):
-                pg3 = pygame.image.load("Images/gr_poissons_rouge.png").convert_alpha()
-                pg3 = pygame.transform.scale(pg3, (self.taille, 50))
-                screen.blit(pg3, self.rect)
+        # on affiche juste l'image déjà chargée
+        screen.blit(self.image, self.rect)
